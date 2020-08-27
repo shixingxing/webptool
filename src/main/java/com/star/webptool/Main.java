bcf17b819f3e39e729ea64a0968679250bb8bf98
shixingxing <shixingxing@yungengxin.com>
2020-08-27 18:13:31 +0800

    swt引入

--- src/main/java/com/star/webptool/Main.java
+++ src/main/java/com/star/webptool/Main.java
@@ -7,6 +7,7 @@ import org.eclipse.swt.widgets.Shell;
 public class Main {
 
     public static void main(String[] args) {
+        //img2webp -loop 0 1.png 2.png 3.png 4.png 5.png 6.png 7.png 8.png 9.png 10.png -o outaaa.webp
 // Create Display
         Display display = new Display();
         // Create Shell (Window) from diplay
